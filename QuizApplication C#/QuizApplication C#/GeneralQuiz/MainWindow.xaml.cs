aa73dcc7db41192d62d9e13c2f946cf867f50060
ASUS <ASUS@HOME-PC>
2023-10-15 22:59:41 +0300

    Add project files.

---
+++ QuizApplication C#/QuizApplication C#/GeneralQuiz/MainWindow.xaml.cs
@@ -0,0 +1,366 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GeneralQuiz
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Quiz q = new Quiz();
+        private List<int> lst = new List<int>();
+        private string ans;
+        private int score = 0;
+        private int number = 1;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            start.Visibility = Visibility.Hidden;
+            quest.Visibility = Visibility.Visible;
+            ans1.Visibility = Visibility.Visible;
+            ans2.Visibility = Visibility.Visible;
+            ans3.Visibility = Visibility.Visible;
+            ans4.Visibility = Visibility.Visible;
+            scoreLbl.Visibility = Visibility.Visible;
+            //
+            int i = this.getRandom();
+            quest.Content = q.getQuestion(i);
+            //
+            ans1.Content = q.getAnswer(i, 1);
+            ans2.Content = q.getAnswer(i, 2);
+            ans3.Content = q.getAnswer(i, 3);
+            ans4.Content = q.getAnswer(i, 4);
+            if (Convert.ToString(ans1.Content).StartsWith("*"))
+            {
+                this.ans = Convert.ToString(ans1.Content).Substring(1, Convert.ToString(ans1.Content).Length - 1);
+                ans1.Content = Convert.ToString(ans1.Content).Substring(1, Convert.ToString(ans1.Content).Length - 1);
+            }
+            else
+            {
+                if (Convert.ToString(ans2.Content).StartsWith("*"))
+                {
+                    this.ans = Convert.ToString(ans2.Content).Substring(1, Convert.ToString(ans2.Content).Length - 1);
+                    ans2.Content = Convert.ToString(ans2.Content).Substring(1, Convert.ToString(ans2.Content).Length - 1);
+                }
+                else
+                {
+                    if (Convert.ToString(ans3.Content).StartsWith("*"))
+                    {
+                        this.ans = Convert.ToString(ans3.Content).Substring(1, Convert.ToString(ans3.Content).Length - 1);
+                        ans3.Content = Convert.ToString(ans3.Content).Substring(1, Convert.ToString(ans3.Content).Length - 1);
+                    }
+                    else
+                    {
+                        this.ans = Convert.ToString(ans4.Content).Substring(1, Convert.ToString(ans4.Content).Length - 1);
+                        ans4.Content = Convert.ToString(ans4.Content).Substring(1, Convert.ToString(ans4.Content).Length - 1);
+                    }
+                }
+            }
+
+
+            lst.Add(i);
+        }
+        public int getRandom()
+        {
+            Random rnd = new Random();
+            int i = rnd.Next(11);
+            if(lst.Contains(i) && lst.Count()<11)
+                while(lst.Contains(i))
+                    i = rnd.Next(0,11);
+            return i;
+        }
+
+        private void ans1_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.number < 11)
+            {
+                this.number++;
+                if (Convert.ToString(ans1.Content) == this.ans)
+                    this.score++;
+                int i = this.getRandom();
+                quest.Content = q.getQuestion(i);
+                //
+                ans1.Content = q.getAnswer(i, 1);
+                ans2.Content = q.getAnswer(i, 2);
+                ans3.Content = q.getAnswer(i, 3);
+                ans4.Content = q.getAnswer(i, 4);
+                if (Convert.ToString(ans1.Content).StartsWith("*"))
+                {
+                    this.ans = Convert.ToString(ans1.Content).Substring(1, Convert.ToString(ans1.Content).Length - 1);
+                    ans1.Content = Convert.ToString(ans1.Content).Substring(1, Convert.ToString(ans1.Content).Length - 1);
+                }
+                else
+                {
+                    if (Convert.ToString(ans2.Content).StartsWith("*"))
+                    {
+                        this.ans = Convert.ToString(ans2.Content).Substring(1, Convert.ToString(ans2.Content).Length - 1);
+                        ans2.Content = Convert.ToString(ans2.Content).Substring(1, Convert.ToString(ans2.Content).Length - 1);
+                    }
+                    else
+                    {
+                        if (Convert.ToString(ans3.Content).StartsWith("*"))
+                        {
+                            this.ans = Convert.ToString(ans3.Content).Substring(1, Convert.ToString(ans3.Content).Length - 1);
+                            ans3.Content = Convert.ToString(ans3.Content).Substring(1, Convert.ToString(ans3.Content).Length - 1);
+                        }
+                        else
+                        {
+                            this.ans = Convert.ToString(ans4.Content).Substring(1, Convert.ToString(ans4.Content).Length - 1);
+                            ans4.Content = Convert.ToString(ans4.Content).Substring(1, Convert.ToString(ans4.Content).Length - 1);
+                        }
+                    }
+                }
+                lst.Add(i);
+                scoreLbl.Content = "Score : "+this.score;
+            }
+            else
+            {
+                quest.Visibility = Visibility.Hidden;
+                ans1.Visibility = Visibility.Hidden;
+                ans2.Visibility = Visibility.Hidden;
+                ans3.Visibility = Visibility.Hidden;
+                ans4.Visibility = Visibility.Hidden;
+                final.Visibility = Visibility.Visible;
+                final.Content ="         Congratulations" + "\n          Your score is " + this.score;
+                restart.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void ans2_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.number < 11)
+            {
+                this.number++;
+                if (Convert.ToString(ans2.Content) == this.ans)
+                    this.score++;
+                int i = this.getRandom();
+                quest.Content = q.getQuestion(i);
+                //
+                ans1.Content = q.getAnswer(i, 1);
+                ans2.Content = q.getAnswer(i, 2);
+                ans3.Content = q.getAnswer(i, 3);
+                ans4.Content = q.getAnswer(i, 4);
+                if (Convert.ToString(ans1.Content).StartsWith("*"))
+                {
+                    this.ans = Convert.ToString(ans1.Content).Substring(1, Convert.ToString(ans1.Content).Length - 1);
+                    ans1.Content = Convert.ToString(ans1.Content).Substring(1, Convert.ToString(ans1.Content).Length - 1);
+                }
+                else
+                {
+                    if (Convert.ToString(ans2.Content).StartsWith("*"))
+                    {
+                        this.ans = Convert.ToString(ans2.Content).Substring(1, Convert.ToString(ans2.Content).Length - 1);
+                        ans2.Content = Convert.ToString(ans2.Content).Substring(1, Convert.ToString(ans2.Content).Length - 1);
+                    }
+                    else
+                    {
+                        if (Convert.ToString(ans3.Content).StartsWith("*"))
+                        {
+                            this.ans = Convert.ToString(ans3.Content).Substring(1, Convert.ToString(ans3.Content).Length - 1);
+                            ans3.Content = Convert.ToString(ans3.Content).Substring(1, Convert.ToString(ans3.Content).Length - 1);
+                        }
+                        else
+                        {
+                            this.ans = Convert.ToString(ans4.Content).Substring(1, Convert.ToString(ans4.Content).Length - 1);
+                            ans4.Content = Convert.ToString(ans4.Content).Substring(1, Convert.ToString(ans4.Content).Length - 1);
+                        }
+                    }
+                }
+                lst.Add(i);
+                scoreLbl.Content = "Score : " + this.score;
+            }
+            else
+            {
+                quest.Visibility = Visibility.Hidden;
+                ans1.Visibility = Visibility.Hidden;
+                ans2.Visibility = Visibility.Hidden;
+                ans3.Visibility = Visibility.Hidden;
+                ans4.Visibility = Visibility.Hidden;
+                final.Visibility = Visibility.Visible;
+                final.Content = "         Congratulations"+"\n          Your score is " + this.score;
+                restart.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void ans3_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.number < 11)
+            {
+                this.number++;
+                if (Convert.ToString(ans3.Content) == this.ans)
+                    this.score++;
+                int i = this.getRandom();
+                quest.Content = q.getQuestion(i);
+                //
+                ans1.Content = q.getAnswer(i, 1);
+                ans2.Content = q.getAnswer(i, 2);
+                ans3.Content = q.getAnswer(i, 3);
+                ans4.Content = q.getAnswer(i, 4);
+                if (Convert.ToString(ans1.Content).StartsWith("*"))
+                {
+                    this.ans = Convert.ToString(ans1.Content).Substring(1, Convert.ToString(ans1.Content).Length - 1);
+                    ans1.Content = Convert.ToString(ans1.Content).Substring(1, Convert.ToString(ans1.Content).Length - 1);
+                }
+                else
+                {
+                    if (Convert.ToString(ans2.Content).StartsWith("*"))
+                    {
+                        this.ans = Convert.ToString(ans2.Content).Substring(1, Convert.ToString(ans2.Content).Length - 1);
+                        ans2.Content = Convert.ToString(ans2.Content).Substring(1, Convert.ToString(ans2.Content).Length - 1);
+                    }
+                    else
+                    {
+                        if (Convert.ToString(ans3.Content).StartsWith("*"))
+                        {
+                            this.ans = Convert.ToString(ans3.Content).Substring(1, Convert.ToString(ans3.Content).Length - 1);
+                            ans3.Content = Convert.ToString(ans3.Content).Substring(1, Convert.ToString(ans3.Content).Length - 1);
+                        }
+                        else
+                        {
+                            this.ans = Convert.ToString(ans4.Content).Substring(1, Convert.ToString(ans4.Content).Length - 1);
+                            ans4.Content = Convert.ToString(ans4.Content).Substring(1, Convert.ToString(ans4.Content).Length - 1);
+                        }
+                    }
+                }
+                lst.Add(i);
+                scoreLbl.Content = "Score : " + this.score;
+            }
+            else
+            {
+                quest.Visibility = Visibility.Hidden;
+                ans1.Visibility = Visibility.Hidden;
+                ans2.Visibility = Visibility.Hidden;
+                ans3.Visibility = Visibility.Hidden;
+                ans4.Visibility = Visibility.Hidden;
+                final.Visibility = Visibility.Visible;
+                final.Content = "         Congratulations"+"\n          Your score is " + this.score;
+                restart.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void ans4_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.number <11)
+            {
+                this.number++;
+                if (Convert.ToString(ans4.Content) == this.ans)
+                    this.score++;
+                int i = this.getRandom();
+                quest.Content = q.getQuestion(i);
+                //
+                ans1.Content = q.getAnswer(i, 1);
+                ans2.Content = q.getAnswer(i, 2);
+                ans3.Content = q.getAnswer(i, 3);
+                ans4.Content = q.getAnswer(i, 4);
+                if (Convert.ToString(ans1.Content).StartsWith("*"))
+                {
+                    this.ans = Convert.ToString(ans1.Content).Substring(1, Convert.ToString(ans1.Content).Length - 1);
+                    ans1.Content = Convert.ToString(ans1.Content).Substring(1, Convert.ToString(ans1.Content).Length - 1);
+                }
+                else
+                {
+                    if (Convert.ToString(ans2.Content).StartsWith("*"))
+                    {
+                        this.ans = Convert.ToString(ans2.Content).Substring(1, Convert.ToString(ans2.Content).Length - 1);
+                        ans2.Content = Convert.ToString(ans2.Content).Substring(1, Convert.ToString(ans2.Content).Length - 1);
+                    }
+                    else
+                    {
+                        if (Convert.ToString(ans3.Content).StartsWith("*"))
+                        {
+                            this.ans = Convert.ToString(ans3.Content).Substring(1, Convert.ToString(ans3.Content).Length - 1);
+                            ans3.Content = Convert.ToString(ans3.Content).Substring(1, Convert.ToString(ans3.Content).Length - 1);
+                        }
+                        else
+                        {
+                            this.ans = Convert.ToString(ans4.Content).Substring(1, Convert.ToString(ans4.Content).Length - 1);
+                            ans4.Content = Convert.ToString(ans4.Content).Substring(1, Convert.ToString(ans4.Content).Length - 1);
+                        }
+                    }
+                }
+                lst.Add(i);
+                scoreLbl.Content = "Score : " + this.score;
+            }
+            else
+            {
+                quest.Visibility = Visibility.Hidden;
+                ans1.Visibility = Visibility.Hidden;
+                ans2.Visibility = Visibility.Hidden;
+                ans3.Visibility = Visibility.Hidden;
+                ans4.Visibility = Visibility.Hidden;
+                final.Visibility = Visibility.Visible;
+                final.Content = "         Congratulations" + "\n          Your score is " + this.score;
+                restart.Visibility = Visibility.Visible;
+               
+            }
+        }
+
+        private void Button_Click2(object sender, RoutedEventArgs e)
+        {
+            this.score = 0;
+            scoreLbl.Content = "Score : "+this.score;
+            this.number = 1;
+            final.Visibility = Visibility.Hidden;
+            lst.Clear();
+            restart.Visibility = Visibility.Hidden;
+            quest.Visibility = Visibility.Visible;
+            ans1.Visibility = Visibility.Visible;
+            ans2.Visibility = Visibility.Visible;
+            ans3.Visibility = Visibility.Visible;
+            ans4.Visibility = Visibility.Visible;
+            scoreLbl.Visibility = Visibility.Visible;
+            //
+            int i = this.getRandom();
+            quest.Content = q.getQuestion(i);
+            //
+            ans1.Content = q.getAnswer(i, 1);
+            ans2.Content = q.getAnswer(i, 2);
+            ans3.Content = q.getAnswer(i, 3);
+            ans4.Content = q.getAnswer(i, 4);
+            if (Convert.ToString(ans1.Content).StartsWith("*"))
+            {
+                this.ans = Convert.ToString(ans1.Content).Substring(1, Convert.ToString(ans1.Content).Length - 1);
+                ans1.Content = Convert.ToString(ans1.Content).Substring(1, Convert.ToString(ans1.Content).Length - 1);
+            }
+            else
+            {
+                if (Convert.ToString(ans2.Content).StartsWith("*"))
+                {
+                    this.ans = Convert.ToString(ans2.Content).Substring(1, Convert.ToString(ans2.Content).Length - 1);
+                    ans2.Content = Convert.ToString(ans2.Content).Substring(1, Convert.ToString(ans2.Content).Length - 1);
+                }
+                else
+                {
+                    if (Convert.ToString(ans3.Content).StartsWith("*"))
+                    {
+                        this.ans = Convert.ToString(ans3.Content).Substring(1, Convert.ToString(ans3.Content).Length - 1);
+                        ans3.Content = Convert.ToString(ans3.Content).Substring(1, Convert.ToString(ans3.Content).Length - 1);
+                    }
+                    else
+                    {
+                        this.ans = Convert.ToString(ans4.Content).Substring(1, Convert.ToString(ans4.Content).Length - 1);
+                        ans4.Content = Convert.ToString(ans4.Content).Substring(1, Convert.ToString(ans4.Content).Length - 1);
+                    }
+                }
+            }
+            lst.Add(i);
+        }
+    }
+}
---
+++ QuizApplication C#/QuizApplication C#/GeneralQuiz/Quiz.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneralQuiz
+{
+    class Quiz
+    {
+        private string[,] questions = new string[20, 5];
+        public Quiz()
+        {
+            questions[0, 0] = "The 2006 World Cup Football Tournament held in";
+            questions[0, 1] = "France";
+            questions[0, 2] = "China";
+            questions[0, 3] = "*Germany";
+            questions[0, 4] = "Brazil";
+            //
+            questions[1, 0] = "The 'Black flag' signifies";
+            questions[1, 1] = "revolution/danger";
+            questions[1, 2] = "*protest";
+            questions[1, 3] = "truce";
+            questions[1, 4] = "peace";
+            //
+            questions[2, 0] = "Robert Koch worked on";
+            questions[2, 1] = "*tuberculosis";
+            questions[2, 2] = "cholera";
+            questions[2, 3] = "malaria";
+            questions[2, 4] = "diabetes";
+            //
+            questions[3, 0] = "The 2002 Commonwealth Games were held in";
+            questions[3, 1] = "*UK";
+            questions[3, 2] = "Canada";
+            questions[3, 3] = "Australia";
+            questions[3, 4] = "Malaysia";
+            //
+            questions[4, 0] = "The 2012 Olympics Games were held in";
+            questions[4, 1] = "New York";
+            questions[4, 2] = "Seul";
+            questions[4, 3] = "*London";
+            questions[4, 4] = "Tokyo";
+            //
+            questions[5, 0] = "Who is the owner of Marvel Studios";
+            questions[5, 1] = "Warner Bros";
+            questions[5, 2] = "*Walt Disney";
+            questions[5, 3] = "21st Century Fox";
+            questions[5, 4] = "Universal Studios";
+            //
+            questions[6, 0] = "Pythagoras was first to ____ the universal validity of geometrical theorem.";
+            questions[6, 1] = "give";
+            questions[6, 2] = "*prove";
+            questions[6, 3] = "both";
+            questions[6, 4] = "None of the above";
+            //
+            questions[7, 0] = "Sulphur is not present in";
+            questions[7, 1] = "iron pyrites";
+            questions[7, 2] = "gypsum";
+            questions[7, 3] = "coal";
+            questions[7, 4] = "*chlorapatite";
+            //
+            questions[8, 0] = "Oscar Awards were instituted in";
+            questions[8, 1] = "1968";
+            questions[8, 2] = "1901";
+            questions[8, 3] = "1965";
+            questions[8, 4] = "*1929";
+            //
+            questions[9, 0] = "Sculpture flourished during";
+            questions[9, 1] = "*Indus valley civilization";
+            questions[9, 2] = "Egyptian civilization";
+            questions[9, 3] = "Chinese civilization";
+            questions[9, 4] = "None of the above";
+            //
+            questions[10, 0] = "Penicillin is widely used as";
+            questions[10, 1] = "an insecticide";
+            questions[10, 2] = "an antiseptic";
+            questions[10, 3] = "a disinfectant";
+            questions[10, 4] = "*an antibiotic";
+        }
+        public string getQuestion(int i)
+        {
+            return questions[i, 0];
+        }
+        public string getAnswer(int i,int j)
+        {
+            return questions[i, j];
+        }
+
+    }
+}
